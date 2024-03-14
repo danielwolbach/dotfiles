ddd5c150e25ba3bb8608f604e87ce3c519afa33b
Daniel Wolbach <77749247+danielwolbach@users.noreply.github.com>
2024-03-14 01:02:33 +0100

    Fix Bash config

--- home/bashrc.sh
+++ home/bashrc.sh
@@ -2,21 +2,24 @@ if [ -f "/etc/bashrc" ]; then
     source "/etc/bashrc"
 fi
 
-if [ -z "$TMUX" ]; then
-    exec tmux new-session -A -s main
+if [[ $- == *i* ]]
+then
+    if [ -z "$TMUX" ]; then
+        exec tmux new-session -A -s main
+    fi
+
+    export PATH="$PATH:~/.local/bin"
+    export PATH="$PATH:~/.cargo/bin"
+
+    export EDITOR="hx"
+    export VISUAL="hx"
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+    export MANROFFOPT="-c"
+
+    export PS1="[\W]$ "
+
+    alias ls="eza --icons"
+    alias ll="eza --icons --long --header --git"
+    alias la="eza --icons --long --header --git --all"
+    alias tree="eza --icons --tree --git-ignore"
 fi
-
-export PATH="$PATH:~/.local/bin"
-export PATH="$PATH:~/.cargo/bin"
-
-export EDITOR="hx"
-export VISUAL="hx"
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-export MANROFFOPT="-c"
-
-export PS1="[\W]$ "
-
-alias ls="eza --icons"
-alias ll="eza --icons --long --header --git"
-alias la="eza --icons --long --header --git --all"
-alias tree="eza --icons --tree --git-ignore"
